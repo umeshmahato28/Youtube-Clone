27f7d839b8ec077ee653cb44768ca12285e01dca
umeshmahato28 <mahatoumesh94@gmail.com>
2023-12-29 19:14:18 +0530

    autoplay

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -19,7 +19,7 @@ const WatchPage = () => {
       <iframe 
         width="1000"
         height="520"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        src={"https://www.youtube.com/embed/" + searchParams.get("v") + "?autoplay=1"}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
